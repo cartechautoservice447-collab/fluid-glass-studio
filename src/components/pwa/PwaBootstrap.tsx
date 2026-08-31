6afc2c262ce674b3a68e5b6e6ae93374fd5f85c4
cartechautoservice447-collab <cartechautoservice447@gmail.com>
2026-08-31 10:37:29 +0530

    Remove unused duplicate PWA bootstrap

--- src/components/pwa/PwaBootstrap.tsx
+++
@@ -1,9 +0,0 @@
-import { useEffect } from "react";
-import { registerPwa } from "@/pwa/register";
-
-export function PwaBootstrap() {
-  useEffect(() => {
-    registerPwa();
-  }, []);
-  return null;
-}
